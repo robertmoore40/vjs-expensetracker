5dec6ab3a171d6f662df3b4b1ce5f241be3f9ed7
robertmoore40 <robertcmoore92@gmail.com>
2020-07-30 09:08:01 -0400

    remove

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ let transactions =
   localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
 
+// Remove transaction by ID
+function removeTransaction(id) {
+    transactions = transactions.filter(transaction => transaction.id !== id);
+  
+    updateLocalStorage();
+  
+    init();
+  }
+
 // App start
 function init() {
     list.innerHTML = '';
