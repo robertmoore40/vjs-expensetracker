2842e9460f4e0821b52a3de01edd51ad163816be
robertmoore40 <robertcmoore92@gmail.com>
2020-08-02 15:34:06 -0400

    added new transactions

--- script.js
+++ script.js
@@ -15,3 +15,30 @@ const localStorageTransactions = JSON.parse(
 let transactions =
   localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
+
+
+  function addTransaction(e) {
+    e.preventDefault();
+  
+    if (text.value.trim() === '' || amount.value.trim() === '') {
+      alert('Please add a text and amount');
+    } else {
+      const transaction = {
+        id: generateID(),
+        text: text.value,
+        amount: +amount.value
+      };
+  
+      transactions.push(transaction);
+  
+      addTransactionDOM(transaction);
+  
+      updateValues();
+  
+      updateLocalStorage();
+  
+      text.value = '';
+      amount.value = '';
+    }
+  }
+
